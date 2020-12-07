fb0d23399494c64990831e563b021f7d55bdfca7
Andrew Simmons <neon.duncan@gmail.com>
2020-12-07 20:40:18 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,40 @@
+var deliLine = ['Grace', 'Toby', 'Rick']
+
+//adds a new person to the line and returns a welcome message with their position in line.
+
+function takeANumber(lineArr, name) {
+  lineArr.push(name)
+  let linePosition = lineArr.length
+  var welcomeMessage = (`Welcome, ${name}. You are number ${linePosition} in line.`)
+  return welcomeMessage
+}
+
+//returns the name of the person next in line and removes them from the line
+function nowServing(lineArr) {
+  var nextInLine = lineArr.shift()
+  if (typeof(nextInLine) === 'string') {
+    return nextInLine 
+  } else {
+    return "There is nobdy waiting to be served!"
+  }
+}
+
+ //missing commas in between
+ //prints the name and place of each person in line
+function currentLine (lineArr) {
+  var message = "The line is currently: "
+  if (lineArr[0] === false) {
+     message = "The line is currently empty."
+  } else {
+    lineArr.forEach(function(item, index) {
+      message += index + 1 + ". " + item + " "
+    })
+  }
+  return message
+}
+
+//console.log(takeANumber(deliLine, "Bob"))
+//console.log(deliLine)
+console.log(nowServing(deliLine))
+
+console.log(currentLine(deliLine))
